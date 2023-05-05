85fc9df90543705b6bec109dbfa2444f031f9286
imane-azzamok <azzamokimane20@gmail.com>
2023-05-05 14:08:21 +0100

    third  push

--- resources/views/components/template1.blade.php
+++ resources/views/components/template1.blade.php
@@ -8,8 +8,9 @@
 </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-5-strong">
         <a class="navbar-brand" href="#">Navbar</a>
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -34,10 +35,17 @@
               </div>
             </li>
           </ul>
-          <div>
+          <div class=" d-flex flex-row ">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </div>
+          <div class='m-2'>
+            <button  type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+             </svg>
+            </button>
+          </div>
+          
         </div>
       </nav>
 
